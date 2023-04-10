4a1fd6419bbedc9801a16910ae11e4cd749a2153
hxiong87 <117489323+hxiong87@users.noreply.github.com>
2023-04-09 23:41:57 -0500

    updated delete

--- src/components/DeleteRoutine.js
+++ src/components/DeleteRoutine.js
@@ -3,24 +3,27 @@ import { deleteRoutine } from '../api'
 import { useNavigate } from "react-router-dom";
 
 export const DeleteRoutine = (routineId) => {
-    const navigate = useNavigate;
-    const handleDelete = async (e) => {
+    const navigate = useNavigate();
+    const handleDelete = async () => {
         console.log('delete is getting hit')
-        e.preventDefault();
+        // e.preventDefault();
        
         const token = window.localStorage.getItem('token')
         deleteRoutine(routineId)
       
         
         console.log('delete routineId', routineId)
-        window.location.reload(); 
-        //  navigate(`/my_routines`)
+        // window.location.reload(); 
+        alert('routine deleted')
+         navigate(`/routines`)
        
     }
 
 
     return (
         <div>
+            
+        
             <label>Delete Routine: </label>
             <button onClick={handleDelete}>Delete</button>
         </div>
